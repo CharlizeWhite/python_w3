b7cfc0b4a1e1744adbab0d0222baf98e156ff457
Charlize_White <guimel1@hotmail.com>
2025-08-28 00:25:23 -0600

    inicio operadores

---
+++ 07.Operadores.py
@@ -0,0 +1,13 @@
+##Operadores
+#los operadores son usados para realizar operaciones en variables
+#y valores
+
+#Python divide a los operadores en los siguientes grupos
+
+#Aritmeticos
+#Asignacion
+#Comparacion
+#Logicos
+#Identidad
+#Membresia
+#Operadores Bitwise
