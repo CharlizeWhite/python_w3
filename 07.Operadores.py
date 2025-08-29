fd2c8f779c0538c0d074c58f5b51778eec08c912
Charlize_White <guimel1@hotmail.com>
2025-08-28 19:00:50 -0600

    avance operadores

--- 07.Operadores.py
+++ 07.Operadores.py
@@ -11,3 +11,31 @@
 #Identidad
 #Membresia
 #Operadores Bitwise
+
+#Operadores Aritmeticos
+# '+' -> suma
+# '-' -> resta
+# '*' -> multiplicacion
+# '/' -> division
+# '%' -> modulus -> retorna el residuo de una division no exacta
+# '**' -> exponencia
+# '//' -> floor division (Division entera ó por el suelo)
+# devuelve solo numeros enteros de una division, descartando decimales.
+
+#Operadores de Asignacion de Python
+# Operador      Ejemplo     Igual que
+# '='           x = 5
+# '+='          x += 3      x = x + 3
+# '-='          x -= 3      x = x - 3
+# '*='          x *= 3      x = x * 3
+# '/='          x /= 3      x = x / 3
+# '%='
+# '//='
+# '**='
+# '&='
+# '|='
+# '^='
+# '>>='
+# '<<='
+# ':='
+
