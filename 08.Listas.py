4a6a8ed7123e972173a2b74510e7a5e14b2ba9c9
Charlize_White <guimel1@hotmail.com>
2025-08-29 23:50:09 -0600

    inicio listas

---
+++ 08.Listas.py
@@ -0,0 +1,11 @@
+##Listas
+
+#Las listas son usadas para almacenar multiples items en una sola variable
+
+#Hay 4 maneras de almacenar 'Colecciones de datos' con diferentes cualidades y usos
+    #Listas
+    #Tuplas
+    #Sets
+    #Diccionarios
+
+#Las listas son creadas usando los corchetes cuadradas
