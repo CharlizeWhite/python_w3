d3fc11679ab10b70404678cb2bcb27570c3109d6
Charlize_White <guimel1@hotmail.com>
2025-09-03 00:08:44 -0600

    inicio de cambio de elementos

---
+++ 10.cambia_elementos_lista.py
@@ -0,0 +1,7 @@
+#Cambia elementos de la lista
+#Para cambiar un valor de un elemento específico, se refiere el número del índice
+
+#Cambio del segundo elemento
+thislist = ["Pedro", "Muñeca", "Rocco"]
+thislist[1] = "Dobby"
+print(thislist)
